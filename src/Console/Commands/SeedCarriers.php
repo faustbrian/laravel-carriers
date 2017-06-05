e94e72b4673fa79c1e9f2e149f219e749cf6e5c8
Brian Faust <faustbrian@users.noreply.github.com>
2017-06-05 12:38:41 +0300

    Apply fixes from StyleCI (#10)
    [ci skip] [skip ci]

--- src/Console/Commands/SeedCarriers.php
+++ src/Console/Commands/SeedCarriers.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Laravel Carriers.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrianFaust\Carriers\Console\Commands;
 
 use BrianFaust\Carriers\Models\Carrier;
