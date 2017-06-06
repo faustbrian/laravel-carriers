0422fc99da56059f0822ac46ae1c63cce5282455
Brian Faust <hello@brianfaust.de>
2017-06-06 10:59:48 +0300

    some comments

--- src/CarriersServiceProvider.php
+++ src/CarriersServiceProvider.php
@@ -30,6 +30,14 @@ class CarriersServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->registerCommands();
+    }
+
+    /**
+     * Register the console commands.
+     */
+    private function registerCommands()
+    {
         $this->commands(Console\Commands\SeedCarriers::class);
     }
 }
